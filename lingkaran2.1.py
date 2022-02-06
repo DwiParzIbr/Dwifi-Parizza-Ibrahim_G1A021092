9a67a3fc3b899029e47e98c1506cef83d1c67f2a
DwiParzIbr <99091149+DwiParzIbr@users.noreply.github.com>
2022-02-06 20:21:31 +0700

    Update lingkaran2.1.py

--- lingkaran2.1.py
+++ lingkaran2.1.py
@@ -1,4 +1,14 @@
+#Ini merupakan salah satu penerapan ADT dalam kehidupan sehari-hari
+#contohnya pada luas lingkaran seperti yang ada dibawah ini
+
 phi = 3.14
+#phi atau π (22/7 atau 3.14) merupakan sebuah ketetapan dalam rumus sebuah lingkaran
+
 r = float(input("Masukkan panjang jari-jari lingkaran: "))
+#r merupakan jari-jari dari sebuah lingkaran atau setengah dari diameter sebuah lingkaran
+
 luas = phi * (r**2)
+#karena luas dari sebuah lingkaran adalah phi*r*r maka dapat dipersingkat menjadi phi*r2
+
 print("Luas lingkaran adalah: "+ str(luas))
+#maka luas sebuah lingkaran bisa didapatkan
